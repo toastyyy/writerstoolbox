dbe85a368249dc1ad2e33cb2d45530344788ebca
Christoph Schreiber <christoph-schreiber@arcor.de>
2013-12-01 23:28:47 +0100

    SQLite-Wrapper implementiert, Grundstruktur hergestellt

---
+++ WritersToolbox/WritersToolbox/dao/DAOImpl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using System.IO;
+using Windows.Storage;
+
+namespace WritersToolbox.dao
+{
+    class DAOImpl : DAOInterface
+    {
+        public static readonly String DB_NAME = "toolbox.db";
+
+
+        private static SQLiteConnection dbConn;
+
+        public static void connect()
+        {
+            String dbPath = Path.Combine(ApplicationData.Current.LocalFolder.Path, DB_NAME);
+            DAOImpl.dbConn = new SQLiteConnection(dbPath);
+        }
+
+        public int countUnsortedNotes()
+        {
+            return 0;
+        }
+    }
+}
---
+++ WritersToolbox/WritersToolbox/dao/DAOInterface.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WritersToolbox.dao
+{
+    interface DAOInterface
+    {
+        /// <summary>
+        /// Gibt die Anzahl der unsortierten Notizen zurueck.
+        /// </summary>
+        /// <returns>Anzahl der unsortierten Notizen</returns>
+        int countUnsortedNotes();
+
+
+    }
+}
