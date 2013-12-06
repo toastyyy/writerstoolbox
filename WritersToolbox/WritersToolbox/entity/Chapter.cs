589685c4847fe2389116dea9d985f81911914250
Samir-Toumi <toumi.sam@gmail.com>
2013-12-06 12:34:34 +0100

    chapter-Klasse bearbeiten

---
+++ WritersToolbox/WritersToolbox/entity/Chapter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace WritersToolbox.entity
+{
+    /// <summary>
+    /// Repraesentiert ein Entity vom Kapitel.
+    /// </summary>
+    class Chapter
+    {
+        
+        public int chapterID { get; set; }
+        public String title { get; set; }
+        String color { get; set; }
+        int chapterNumber { get; set; }
+        int tomeID { get; set; }
+    }
+}
