59308b378bc24cd88957245d07036154677b34d2
Samir-Toumi <toumi.sam@gmail.com>
2013-12-12 22:07:33 +0100

    Globale Änderungen

---
+++ WritersToolbox/WritersToolbox/viewmodels/MemoryNoteViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WritersToolbox.entity;
+namespace WritersToolbox.viewmodels
+{
+    class MemoryNoteViewModel
+    {
+
+        public void save(MemoryNote obj_memoryNote)
+        {
+
+        }
+
+        public void saveAs(MemoryNote obj_memoryNote)
+        {
+        }
+
+
+    }
+}
