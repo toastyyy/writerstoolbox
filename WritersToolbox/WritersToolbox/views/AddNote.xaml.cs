a2577c8fe13a418097b279230ad438dafc66112e
Samir-Toumi <toumi.sam@gmail.com>
2013-12-17 23:27:25 +0100

    foto_löschen_von_addnote

--- WritersToolbox/WritersToolbox/views/AddNote.xaml.cs
+++ WritersToolbox/WritersToolbox/views/AddNote.xaml.cs
@@ -21,8 +21,7 @@ namespace WritersToolbox.views
         PhotoChooserTask photoChooserTask;
         Image img;
         HashSet<Rectangle> rectangles;
-        List<Image> t = new List<Image>();
-        Image act_img;
+        List<Image> imageList = new List<Image>();
         int index = -1;
         public AddNote()
         {
@@ -30,7 +29,7 @@ namespace WritersToolbox.views
             photoChooserTask = new PhotoChooserTask();
             photoChooserTask.Completed += new EventHandler<PhotoResult>(photoChooserTask_Completed);
             rectangles = new HashSet<Rectangle>();
-            
+            detailsNote.VerticalScrollBarVisibility = ScrollBarVisibility.Auto;
         }
 
         private void saveButton_Click(object sender, RoutedEventArgs e)
@@ -47,13 +46,14 @@ namespace WritersToolbox.views
                 WriteableBitmap b = new WriteableBitmap(bi);
                 img =  new Image();
                 img.Source = b;
-                img.ManipulationDelta += new EventHandler<ManipulationDeltaEventArgs>(img_ManipulationDelta);
+                img.Hold += new EventHandler<GestureEventArgs>(img_hold);
                 img.Height = 150;
                 img.Width = 200;
                 Canvas.SetLeft(img, Canvas.GetLeft(detailsNote) + 40);
                 Canvas.SetTop(img, Canvas.GetTop(detailsNote) + 50);
                 canvasNote.Children.Add(img);
-                t.Add(img);
+                imageList.Add(img);
+                index++;
             }
         }
 
@@ -62,24 +62,31 @@ namespace WritersToolbox.views
 
         }
 
-
+        private void img_hold(object sender, GestureEventArgs e)
+        {
+            MessageBoxResult m = MessageBox.Show("möchten sie es löschen!?", "löschen", MessageBoxButton.OKCancel);
+            if (m == MessageBoxResult.OK)
+            {
+                canvasNote.Children.Remove(imageList[index]);
+            }
+        }
         private void detailsNote_ManipulationDelta(object sender, System.Windows.Input.ManipulationDeltaEventArgs e)
         {
             if (index >= 0)
             {
-                Image img = t[index];
+                Image temp_img = imageList[index];
                 if (e.PinchManipulation != null)
                 {
                     CompositeTransform ctf = new CompositeTransform();
-                    img.RenderTransform = ctf;
-                    var transform = (CompositeTransform)img.RenderTransform;
-                    var x = Canvas.GetLeft(img);
-                    var y = Canvas.GetTop(img);
+                    temp_img.RenderTransform = ctf;
+                    var transform = (CompositeTransform)temp_img.RenderTransform;
+                    var x = Canvas.GetLeft(temp_img);
+                    var y = Canvas.GetTop(temp_img);
                     // Scale Manipulation
                     if (x + e.DeltaManipulation.Translation.X > Canvas.GetLeft(detailsNote)
                             && y + e.DeltaManipulation.Translation.Y > Canvas.GetTop(detailsNote)
-                            && x + e.DeltaManipulation.Translation.X < Canvas.GetLeft(detailsNote) + img.Width
-                            && y + e.DeltaManipulation.Translation.Y < Canvas.GetTop(detailsNote) + img.Height + 100)
+                            && x + e.DeltaManipulation.Translation.X < Canvas.GetLeft(detailsNote) + temp_img.Width
+                            && y + e.DeltaManipulation.Translation.Y < Canvas.GetTop(detailsNote) + temp_img.Height + 100)
                     {
                         transform.ScaleX = e.PinchManipulation.CumulativeScale;
                         transform.ScaleY = e.PinchManipulation.CumulativeScale;
@@ -96,37 +103,21 @@ namespace WritersToolbox.views
                 }
                 if (e.DeltaManipulation != null)
                 {
-                    var x = Canvas.GetLeft(img);
-                    var y = Canvas.GetTop(img);
+                    var x = Canvas.GetLeft(temp_img);
+                    var y = Canvas.GetTop(temp_img);
                     if (x + e.DeltaManipulation.Translation.X > Canvas.GetLeft(canvasNote)
                         && y + e.DeltaManipulation.Translation.Y > Canvas.GetTop(canvasNote)
-                        && x + e.DeltaManipulation.Translation.X < Canvas.GetLeft(canvasNote) + img.Width
-                        && y + e.DeltaManipulation.Translation.Y < Canvas.GetTop(canvasNote) + img.Height + 100)
+                        && x + e.DeltaManipulation.Translation.X < Canvas.GetLeft(canvasNote) + temp_img.Width
+                        && y + e.DeltaManipulation.Translation.Y < Canvas.GetTop(canvasNote) + temp_img.Height + 100)
                     {
-                        Canvas.SetLeft(img, x + e.DeltaManipulation.Translation.X);
-                        Canvas.SetTop(img, y + e.DeltaManipulation.Translation.Y);
+                        Canvas.SetLeft(temp_img, x + e.DeltaManipulation.Translation.X);
+                        Canvas.SetTop(temp_img, y + e.DeltaManipulation.Translation.Y);
                     }
 
                     e.Handled = true;
                 }
-            }            
-        }
-
-        private void detailsNote_Tap(object sender, GestureEventArgs e)
-        {
-            Point p = e.GetPosition(detailsNote);
-            Point p1;
-            for (int i = t.Count - 1; i >= 0; i--)
-            {
-                var x1 = Canvas.GetLeft(t[i]);
-                var y1 = Canvas.GetTop(t[i]);
-                p1 = new Point(x1, y1);
-                if (p.X > p1.X && p.Y > p1.Y && p.X < p1.X + t[i].Width && p.Y < p1.Y + t[i].Height)
-                {
-                    index = i;
-                    break;
-                }
             }
+            
         }
 
         private void detailsNote_GotFocus(object sender, RoutedEventArgs e)
@@ -134,10 +125,45 @@ namespace WritersToolbox.views
             detailsNote.Background = new SolidColorBrush(Colors.Transparent);
         }
 
-        //private void detailsNote_GotFocus(object sender, RoutedEventArgs e)
-        //{
-        //    detailsNote.Background. = Colors.Transparent;
-        //}
+        private void detailsNote_DoubleTap(object sender, GestureEventArgs e)
+        {
+            saveButton.Focus();
+            Point p = e.GetPosition(detailsNote);
+            Point p1;
+            for (int i = imageList.Count - 1; i >= 0; i--)
+            {
+                var x1 = Canvas.GetLeft(imageList[i]);
+                var y1 = Canvas.GetTop(imageList[i]);
+                p1 = new Point(x1, y1);
+                if (p.X > p1.X && p.Y > p1.Y && p.X < p1.X + imageList[i].Width && p.Y < p1.Y + imageList[i].Height)
+                {
+                    index = i;
+                    break;
+                }
+            }          
+        }
 
+        private void detailsNote_Tap(object sender, GestureEventArgs e)
+        {
+            detailsNote.Focus();
+        }
+
+        private void detailsNote_Hold(object sender, GestureEventArgs e)
+        {
+            if (index >= 0)
+            {
+                MessageBoxResult m = MessageBox.Show("möchten sie es löschen!?", "löschen", MessageBoxButton.OKCancel);
+                if (m == MessageBoxResult.OK)
+                {
+                    canvasNote.Children.Remove(imageList[index]);
+                }
+            }          
+        }
+
+        private void detailsNote_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+            //this.ScrollBar.
+            //this.ScrollBar.UpdateLayout();
+        }
     }
 }
