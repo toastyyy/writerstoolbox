44984f48b6ae5f33b59aa9b3b91f745f9ee3c803
JulsM <julian_maurer@gmx.de>
2013-12-23 17:30:17 +0100

    Grüne Farbe geändert, Typen-Überblick hinzugefügt, zoom-Geste(ungetestet), Data-Template für LongList Typen

--- WritersToolbox/WritersToolbox/views/Types.xaml.cs
+++ WritersToolbox/WritersToolbox/views/Types.xaml.cs
@@ -8,13 +8,45 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 
+
 namespace WritersToolbox.views
 {
     public partial class Types : PhoneApplicationPage
     {
+        
+        
+        
+
         public Types()
         {
             InitializeComponent();
+           
         }
+
+        
+
+        private void pinch_out(object sender, System.Windows.Input.ManipulationDeltaEventArgs e)
+        {
+            
+            if(e.PinchManipulation != null)
+            {
+                if (e.PinchManipulation.CumulativeScale > 1d)
+                {
+                    System.Diagnostics.Debug.WriteLine("Zoomout");
+                    NavigationService.Navigate(new Uri("/views/TypesOverview.xaml", UriKind.Relative));
+                } else
+                    System.Diagnostics.Debug.WriteLine("Zoomin");
+                                
+            }
+            
+            
+        }
+
+        private void navUeberblick(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/views/TypesOverview.xaml", UriKind.Relative));
+        }
+
+        
     }
 }
