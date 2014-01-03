90e0cacdfa60f31707652e2fcf03b79b8a7f0f23
JulsM <julian_maurer@gmx.de>
2014-01-03 19:31:55 +0100

    nur kleine Umbenennung

--- WritersToolbox/WritersToolbox/views/Types.xaml.cs
+++ WritersToolbox/WritersToolbox/views/Types.xaml.cs
@@ -130,7 +130,7 @@ namespace WritersToolbox.views
                 var indexParsed = int.Parse(item);
                 PivotMain.SelectedIndex = indexParsed - 1;
             }
-            else if (PhoneApplicationService.Current.State.ContainsKey("New"))
+            else if (PhoneApplicationService.Current.State.ContainsKey("NewType"))
             {
                 PivotMain.SelectedIndex = PivotMain.Items.Count - 2;
             }
