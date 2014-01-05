889d67fd51b8a84401bd6d72611b492a3d055266
Christoph Schreiber <christoph-schreiber@arcor.de>
2014-01-05 12:59:15 +0100

    Werke: Viewmodel, view, datawrapper, databinding

---
+++ WritersToolbox/WritersToolbox/datawrapper/Tome.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WritersToolbox.datawrapper
+{
+    public class Tome
+    {
+        public int tomeID { get; set; }
+        public String title { get; set; }
+        public int tomeNumber { get; set; }
+        public DateTime addedDate { get; set; }
+        public DateTime updatedDate { get; set; }
+        public List<Chapter> chapters { get; set; }
+        public Book book { get; set; }
+        public Boolean deleted { get; set; }
+    }
+}
---
+++ WritersToolbox/WritersToolbox/viewmodels/BooksViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WritersToolbox.models;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Controls;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using Microsoft.Phone.Controls;
+
+namespace WritersToolbox.viewmodels
+{
+    
+    public class BooksViewModel
+    {
+        public ObservableCollection<datawrapper.Book> Books { get; set; }
+        private Boolean dataLoaded { get; set; }
+        private WritersToolboxDatebase wtb;
+        private Table<Book> tableBook;
+        private Table<Tome> tableTome;
+        public BooksViewModel() {
+            dataLoaded = false;
+            wtb = WritersToolboxDatebase.getInstance();
+            tableBook = wtb.GetTable<Book>();
+            tableTome = wtb.GetTable<Tome>();
+        }
+
+        public Boolean isDataLoaded() 
+        {
+            return dataLoaded;
+        }
+
+        public void loadData() 
+        {
+            var sqlBooks = from b in tableBook
+                                   select b;
+
+            ObservableCollection<datawrapper.Book> tmpBooks = new ObservableCollection<datawrapper.Book>();
+
+            foreach(var b in sqlBooks) 
+            {
+                List<datawrapper.Tome> tmpTomes = new List<datawrapper.Tome>();
+                var sqlTomes = from t in tableTome
+                               where t.obj_book.bookID == b.bookID
+                               select t;
+
+                foreach(var t in sqlTomes) 
+                {
+                    datawrapper.Tome tome = new datawrapper.Tome() { 
+                        addedDate = t.addedDate,
+                        deleted = t.deleted,
+                        title = t.title,
+                        tomeID = t.tomeID,
+                        tomeNumber = t.tomeNumber,
+                        updatedDate = t.updatedDate
+                    };
+
+                    tmpTomes.Add(tome);
+                }
+                datawrapper.Book book = new datawrapper.Book() {
+                    addedDate = b.addedDate,
+                    bookID = b.bookID,
+                    bookType = null,
+                    name = b.name,
+                    tomes = tmpTomes,
+                    updatedDate = b.updatedDate
+                };
+                tmpBooks.Add(book);
+            }
+
+            this.Books = tmpBooks;
+        }
+    }
+}
