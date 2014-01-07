2ec7ba60632e0dd7a3226182eaec71fb900e3a41
Christoph Schreiber <christoph-schreiber@arcor.de>
2014-01-07 14:55:00 +0100

    book funktionalität

---
+++ WritersToolbox/WritersToolbox/converter/DateConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WritersToolbox.converter
+{
+    public class DateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            DateTime val = (DateTime)value;
+
+            String pre = ((String)parameter).Replace("-"," ");
+            return pre + " " + val.ToString("dd.MM.yyyy");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
