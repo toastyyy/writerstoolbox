d611740782d5f52284952f25135912a4649e5d83
JulsM <julian_maurer@gmx.de>
2014-02-02 00:49:44 +0100

    neuer colorpicker (unvollständig), changebook.xaml included

---
+++ WritersToolbox/WritersToolbox/converter/ColorToBrushConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WritersToolbox.converter
+{
+    public class ColorToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value != null)
+            {
+                return new SolidColorBrush((Color)(value));
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    } 
+}
--- WritersToolbox/WritersToolbox/views/AddTypeObject.xaml.cs
+++ WritersToolbox/WritersToolbox/views/AddTypeObject.xaml.cs
@@ -16,6 +16,51 @@ namespace WritersToolbox.views
     {
         private int typeID;
 
+        
+
+        static uint[] uintColors =
+{ 
+	0xFFFFE135,0xFFFFFF66,0xFF008A00,0xFF32CD32,0xFF00FF7F,0xFF808000,0xFFFF0000,0xFFFF4500,
+	0xFFFF8C00,
+	0xFFFF7F50,0xFFDC143C,0xFFFF1493,0xFFB22222,0xFFC71585,
+	0xFFDA70D6,0xFF000080,0xFF4B0082,
+	0xFF800080,0xFF483D8B,0xFFADD8E6,0xFF20B2AA,0xFF008080
+};
+
+        private void ColorPickerPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<ColorItem> item = new List<ColorItem>();
+            for (int i = 0; i < uintColors.Length; i++)
+            {
+                item.Add(new ColorItem() {Color = ConvertColor(uintColors[i]) });
+            };
+
+            listBox.ItemsSource = item; //Fill ItemSource with all colors
+        }
+
+        private Color ConvertColor(uint uintCol)
+        {
+            byte A = (byte)((uintCol & 0xFF000000) >> 24);
+            byte R = (byte)((uintCol & 0x00FF0000) >> 16);
+            byte G = (byte)((uintCol & 0x0000FF00) >> 8);
+            byte B = (byte)((uintCol & 0x000000FF) >> 0);
+
+            return Color.FromArgb(A, R, G, B); ;
+        }
+
+        public class ColorItem
+        {
+            public Color Color { get; set; }
+        }
+
+        private void lstColor_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Count > 0)
+            {
+                //(Application.Current as App).CurrentColorItem = ((ColorItem)e.AddedItems[0]);
+            }
+        }
+
         public AddTypeObject()
         {
             InitializeComponent();
@@ -28,23 +73,23 @@ namespace WritersToolbox.views
         /// <param name="e"></param>
         private void SaveTypeObject(object sender, EventArgs e)
         {
-            Color c = slider.Color;
+            //Color c = slider.Color;
 
-            String r = c.R.ToString("X2");
-            String g = c.G.ToString("X2");
-            String b = c.B.ToString("X2");
+            //String r = c.R.ToString("X2");
+            //String g = c.G.ToString("X2");
+            //String b = c.B.ToString("X2");
 
-            String color = "#" + r + g + b;
-            String name = toName.Text;
+            //String color = "#" + r + g + b;
+            //String name = toName.Text;
 
-            try 
-            {
-                Types.types_VM.createTypeObject(name, color, "", typeID);
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine("Objekt konnte nicht erstellt werden");
-            }
+            //try 
+            //{
+            //    Types.types_VM.createTypeObject(name, color, "", typeID);
+            //}
+            //catch(Exception ex)
+            //{
+            //    Console.WriteLine("Objekt konnte nicht erstellt werden");
+            //}
 
             NavigationService.GoBack();
 
@@ -80,7 +125,7 @@ namespace WritersToolbox.views
                 var item = NavigationContext.QueryString["item"];
                 var indexParsed = int.Parse(item);
                 toName.Text = Types.Types_VM.getNameForTypeObject(indexParsed);
-                slider.Color = Types.Types_VM.getColorForTypeObject(indexParsed);
+                //slider.Color = Types.Types_VM.getColorForTypeObject(indexParsed);
                 Title.Text = "Objekt ändern";
             }
         }
