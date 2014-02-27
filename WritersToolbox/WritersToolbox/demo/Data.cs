45eefd86ac5eb9636a50885acb90ef89545b24dc
Christoph Schreiber <christoph-schreiber@arcor.de>
2014-02-27 15:03:28 +0100

    events finaltext (richtextcontrol) bearbeiten von finaltext

--- WritersToolbox/WritersToolbox/demo/Data.cs
+++ WritersToolbox/WritersToolbox/demo/Data.cs
@@ -104,7 +104,14 @@ namespace WritersToolbox.demo
             { 
                 title = "Haus der Dursleys",
                 obj_Chapter = c1,
-                finaltext = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+                finaltext = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore" +
+                                                    "because they just didn't hold with such nonsense. Mr. Dursley was the director of a firm called Grunnings, which made drills. He was a big, beefy man with hardly any neck," +
+                                    "although he did have a very large mustache. Mrs. Dursley was thin and blonde and had nearly twice the usual amount of neck, which came in very useful as she spent so much of her" +
+                                    "time craning over garden fences, spying on the neighbors. The Dursleys had a small son called Dudley and in their opinion there was no finer boy anywhere." +
+                                    "The Dursleys had everything they wanted, but they also had a secret, and their greatest fear was that somebody would discover it. They didn't think they could bear it" +
+                "if anyone found out about the Potters. Mrs. Potter was Mrs. Dursley's sister, but they hadn't met for several years; in fact, Mrs. Dursley pretended she didn't have a sister, because her sister and" +
+                "her good-for-nothing husband were as unDursleyish as it was possible to be. The Dursleys shuddered to think what the neighbors would say if the Potters arrived in the street. The Dursleys knew that the" + 
+                "et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
             };
 
             Event e2 = new Event()
@@ -358,11 +365,14 @@ namespace WritersToolbox.demo
                                     "although he did have a very large mustache. Mrs. Dursley was thin and blonde and had nearly twice the usual amount of neck, which came in very useful as she spent so much of her" + 
                                     "time craning over garden fences, spying on the neighbors. The Dursleys had a small son called Dudley and in their opinion there was no finer boy anywhere." + 
                                     "The Dursleys had everything they wanted, but they also had a secret, and their greatest fear was that somebody would discover it. They didn't think they could bear it" +
+                                    "because they just didn't hold with such nonsense. Mr. Dursley was the director of a firm called Grunnings, which made drills. He was a big, beefy man with hardly any neck," + 
+                                    "although he did have a very large mustache. Mrs. Dursley was thin and blonde and had nearly twice the usual amount of neck, which came in very useful as she spent so much of her" + 
+                                    "time craning over garden fences, spying on the neighbors. The Dursleys had a small son called Dudley and in their opinion there was no finer boy anywhere." + 
+                                    "The Dursleys had everything they wanted, but they also had a secret, and their greatest fear was that somebody would discover it. They didn't think they could bear it" +
                 "if anyone found out about the Potters. Mrs. Potter was Mrs. Dursley's sister, but they hadn't met for several years; in fact, Mrs. Dursley pretended she didn't have a sister, because her sister and" + 
                 "her good-for-nothing husband were as unDursleyish as it was possible to be. The Dursleys shuddered to think what the neighbors would say if the Potters arrived in the street. The Dursleys knew that the" + 
                 "Potters had a small son, too, but they had never even seen him. This boy was another good reason for keeping the Potters away; they didn't want Dudley mixing with a child like that. " + 
-                "When Mr. and Mrs. Dursley woke up on the dull, gray Tuesday our story starts, there was nothing about the cloudy sky outside to suggest that strange and mysterious things would soon be happening " + 
-                "all over the country. Mr. Dursley hummed as he picked out his most boring tie for work, and Mrs. Dursley gossiped away happily as she wrestled a screaming Dudley into his high chair. None of them noticed a large, tawny owl flutter past the window.",
+                "When Mr. and Mrs. Dursley woke up on the dull, gray Tuesday our story starts, there was nothing about the cloudy sky outside to suggest that strange and mysterious things would soon be happening ",
 　　                title = i.ToString(),
                     associated = true,
                     tags = "harry|potter|test",
--- WritersToolbox/WritersToolbox/views/EventDetail.xaml.cs
+++ WritersToolbox/WritersToolbox/views/EventDetail.xaml.cs
@@ -8,27 +8,218 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using WritersToolbox.viewmodels;
+using Microsoft.Phone.Tasks;
+using System.Windows.Media.Imaging;
+using System.Text.RegularExpressions;
 
 namespace WritersToolbox.views
 {
     public partial class EventDetail : PhoneApplicationPage
     {
         private EventDetailViewModel edvm = null;
+        private PhotoChooserTask photoChooserTask;
+        private int photochooserSelStart = -1;
+        private int photochooserSelLength = 0;
         public EventDetail()
         {
             InitializeComponent();
+            photoChooserTask = new PhotoChooserTask();
+            photoChooserTask.Completed += new EventHandler<PhotoResult>(photoChooserTask_Completed);
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
-            if (NavigationContext.QueryString.ContainsKey("eventID "))
+            if (PhoneApplicationService.Current.State.ContainsKey("preventUpdate") &&
+                (Boolean)PhoneApplicationService.Current.State["preventUpdate"] == true)
             {
-                int eID = int.Parse(NavigationContext.QueryString["eventID "]);
+                PhoneApplicationService.Current.State["preventUpdate"] = false;
+                return;
+            }
+
+            if (NavigationContext.QueryString.ContainsKey("eventID"))
+            {
+                int eID = int.Parse(NavigationContext.QueryString["eventID"]);
                 this.edvm = new EventDetailViewModel(eID);
                 this.edvm.LoadData();
                 this.DataContext = this.edvm.Event;
+                try
+                {
+                    this.tFinalText.Xaml = this.parseRichTextFormat(this.edvm.Event.finaltext);
+                }
+                catch (Exception ex) {
+                    this.tFinalText.Xaml = this.parsePlainText(this.edvm.Event.finaltext);
+                }
+                
             }
+        }
+
+        private void Pivot_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+
+        private void tFinalText_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            this.editFinaltextGrid.Visibility = Visibility.Visible;
+            this.textBoxFinalText.Visibility = Visibility.Visible;
+
+            //workaround fuer tastatur
+            this.textBoxFinalText.Focus();
+            this.WorkaroundButton.Focus();
+            this.textBoxFinalText.Focus();
+            this.createTextEditApplicationBar();
+        }
+
+        private void insertBold(object sender, System.EventArgs e)
+        {
+            int selStart = this.textBoxFinalText.SelectionStart;
+            int selLength = this.textBoxFinalText.SelectionLength;
+            String text = this.textBoxFinalText.Text;
+            if (selLength == 0)
+            {
+                String newText = text.Substring(0, selStart)
+                                + "<b></b>"
+                                + text.Substring(selStart);
+                this.textBoxFinalText.Text = newText;
+                this.textBoxFinalText.SelectionStart = selStart + 3;
+            }
+            else {
+                String newText = text.Substring(0, selStart)
+                                + "<b>" + text.Substring(selStart, selLength) + "</b>"
+                                + text.Substring(selStart + selLength);
+                this.textBoxFinalText.Text = newText;
+                this.textBoxFinalText.SelectionStart = selStart + 3;
+                this.textBoxFinalText.SelectionLength = selLength; 
+            }
+        }
+
+        private void insertItalic(object sender, System.EventArgs e)
+        {
+            int selStart = this.textBoxFinalText.SelectionStart;
+            int selLength = this.textBoxFinalText.SelectionLength;
+            String text = this.textBoxFinalText.Text;
+            if (selLength == 0)
+            {
+                String newText = text.Substring(0, selStart)
+                                + "<i></i>"
+                                + text.Substring(selStart);
+                this.textBoxFinalText.Text = newText;
+                this.textBoxFinalText.SelectionStart = selStart + 3;
+            }
+            else
+            {
+                String newText = text.Substring(0, selStart)
+                                + "<i>" + text.Substring(selStart, selLength) + "</i>"
+                                + text.Substring(selStart + selLength);
+                this.textBoxFinalText.Text = newText;
+                this.textBoxFinalText.SelectionStart = selStart + 3;
+                this.textBoxFinalText.SelectionLength = selLength;
+            }
+        }
+
+        private void insertUnderline(object sender, System.EventArgs e) {
+            int selStart = this.textBoxFinalText.SelectionStart;
+            int selLength = this.textBoxFinalText.SelectionLength;
+            String text = this.textBoxFinalText.Text;
+            if (selLength == 0)
+            {
+                String newText = text.Substring(0, selStart)
+                                + "<u></u>"
+                                + text.Substring(selStart);
+                this.textBoxFinalText.Text = newText;
+                this.textBoxFinalText.SelectionStart = selStart + 3;
+            }
+            else
+            {
+                String newText = text.Substring(0, selStart)
+                                + "<u>" + text.Substring(selStart, selLength) + "</u>"
+                                + text.Substring(selStart + selLength);
+                this.textBoxFinalText.Text = newText;
+                this.textBoxFinalText.SelectionStart = selStart + 3;
+                this.textBoxFinalText.SelectionLength = selLength;
+            }
+        }
+
+        private void createTextEditApplicationBar() {
+            ApplicationBar.Buttons.Clear();
+            ApplicationBarIconButton boldButton = new ApplicationBarIconButton(new Uri("/icons/bold.png", UriKind.RelativeOrAbsolute));
+            boldButton.Text = "fett";
+            boldButton.Click += insertBold;
+            ApplicationBar.Buttons.Add(boldButton);
+            ApplicationBarIconButton italicButton = new ApplicationBarIconButton(new Uri("/icons/italic.png", UriKind.RelativeOrAbsolute));
+            italicButton.Text = "kursiv";
+            italicButton.Click += insertItalic;
+            ApplicationBar.Buttons.Add(italicButton);
+            ApplicationBarIconButton underlineButton = new ApplicationBarIconButton(new Uri("/icons/underline.png", UriKind.RelativeOrAbsolute));
+            underlineButton.Text = "unterstrichen";
+            underlineButton.Click += insertUnderline;
+            ApplicationBar.Buttons.Add(underlineButton);
+            ApplicationBarIconButton saveButton = new ApplicationBarIconButton(new Uri("/icons/save.png", UriKind.RelativeOrAbsolute));
+            saveButton.Text = "speichern";
+            saveButton.Click += saveFinaltextChanges;
+            ApplicationBar.Buttons.Add(saveButton);
+        }
+
+        private void restoreStandardApplicationBar() {
+            ApplicationBar.Buttons.Clear();
+            ApplicationBarIconButton saveButton = new ApplicationBarIconButton(new Uri("/icons/save.png", UriKind.RelativeOrAbsolute));
+            saveButton.Text = "speichern";
+            ApplicationBar.Buttons.Add(saveButton);
+            ApplicationBarIconButton cancelButton = new ApplicationBarIconButton(new Uri("/icons/cancel.png", UriKind.RelativeOrAbsolute));
+            cancelButton.Text = "abbrechen";
+            ApplicationBar.Buttons.Add(cancelButton);
+        }
+
+        private void saveFinaltextChanges(object sender, System.EventArgs e)
+        {
+            this.restoreStandardApplicationBar();
+            this.edvm.updateFinaltext(this.textBoxFinalText.Text);
+            this.DataContext = null;
+            this.DataContext = this.edvm.Event;
+            this.editFinaltextGrid.Visibility = Visibility.Collapsed;
+            this.textBoxFinalText.Visibility = Visibility.Collapsed;
+            try
+            {
+                this.tFinalText.Xaml = this.parseRichTextFormat(this.edvm.Event.finaltext);
+            }
+            catch (Exception ex)
+            {
+                this.tFinalText.Xaml = this.parsePlainText(this.edvm.Event.finaltext);
+            }
+        }
+
+        private void photoChooserTask_Completed(object sender, PhotoResult e)
+        {
+            if (e.TaskResult == TaskResult.OK)
+            {
+                String curText = this.textBoxFinalText.Text;
+                this.textBoxFinalText.Text =
+                    curText.Substring(0, this.photochooserSelStart)
+                    + "<p>" + e.OriginalFileName.Replace(@"\", "/") + "</p>" +
+                    curText.Substring(this.photochooserSelStart + this.photochooserSelLength);
+            }
+        }
+
+        private String parseRichTextFormat(String plain) {
+
+            String retXaml = "";
+            String text = plain;
+            text = Regex.Replace(text, @"<b>(\w+)</b>", "<Bold>$1</Bold>");
+            text = Regex.Replace(text, @"<i>(\w+)</i>", "<Italic>$1</Italic>");
+            text = Regex.Replace(text, @"<u>(\w+)</u>", "<Underline>$1</Underline>");
+
+            retXaml = 
+                @"<Section xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation"">" +
+                "<Paragraph>" + text + "</Paragraph>" + 
+                "</Section>";
+            return retXaml;
+        }
+
+        private String parsePlainText(String xaml) {
+            String ret = xaml.Replace("<", "&lt;").Replace(">", "&gt;");
+            return @"<Section xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation"">" +
+                "<Paragraph>" + ret + "</Paragraph>" +
+                "</Section>";
         }
     }
 }
